80421c311daeb0c7257444e76330cc33f85c7c66
Maxim Polyakov <polyakovmaxim05@gmail.com>
2022-11-22 10:42:58 -0500

    commit tree 66f4a88a80dd2fe1510683c575f94df8b8fee44e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,25 @@ require 'pry'
 
 class String
 
+  attr_accessor :sentence
+
+  def initialize sentence
+    @sentence = sentence
+  end
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/["aA"]+/)
   end
 end
